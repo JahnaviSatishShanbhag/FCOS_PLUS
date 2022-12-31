c3c51605fc993ef13642bc3b6471329a7238f01b
JahnaviSatishShanbhag <71307702+JahnaviSatishShanbhag@users.noreply.github.com>
2022-12-31 15:51:08 +0530

    Update paths_catalog.py

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -5,7 +5,7 @@ import os
 
 
 class DatasetCatalog(object):
-    DATA_DIR = "datasets"
+    DATA_DIR = "/kaggle/input/doclaynet"
     DATASETS = {
         "coco_2017_train": {
             "img_dir": "coco/train2017",
@@ -24,20 +24,20 @@ class DatasetCatalog(object):
             "ann_file": "/kaggle/input/doclaynet/COCO/val.json"
         },
         "coco_2014_train": {
-            "img_dir": "coco/train2014",
-            "ann_file": "coco/annotations/instances_train2014.json"
+            "img_dir": "PNG",
+            "ann_file": "COCO/train.json"
         },
         "coco_2014_val": {
             "img_dir": "coco/val2014",
             "ann_file": "coco/annotations/instances_val2014.json"
         },
         "coco_2014_minival": {
-            "img_dir": "coco/val2014",
-            "ann_file": "coco/annotations/instances_minival2014.json"
+            "img_dir": "PNG",
+            "ann_file": "COCO/test.json"
         },
         "coco_2014_valminusminival": {
-            "img_dir": "coco/val2014",
-            "ann_file": "coco/annotations/instances_valminusminival2014.json"
+            "img_dir": "PNG",
+            "ann_file": "COCO/val.json"
         },
         "keypoints_coco_2014_train": {
             "img_dir": "coco/train2014",
